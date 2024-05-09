18ed3b25c394af82161d5d0e7c24d25d7e3ebd7d
marianelavq <marianela.vasquez@gmail.com>
2024-05-09 13:31:08 +0200

    Agraga comentarios en 012_Rango_de_indices.py

--- 04_PYTHON/012_Rango_de_indices.py
+++ 04_PYTHON/012_Rango_de_indices.py
@@ -1,18 +1,19 @@
-# Rango de indices. Ej Crear una lista de elementos y devolver el segundo, tercer y cuarto elemento.
+# Rango de indices: Se puede especificar un rango de índices si le especificamos dónde comenzar y dónde terminar el rango. Al especificar un rango, el valor de retorno será una nueva lista con los elementos especificados. 
+# Ej Crear una lista de elementos y devolver el segundo, tercer y cuarto elemento.
 
 thislist = ["Manzana", "plátano", "cereza", "naranja", "kiwi", "melon", "mango"]
 print(thislist[2:5])
 
 # Nota: La búsqueda comenzará en el índice 2 (incluido) y finalizará en el índice 5 (no incluido). Recordemos que el primer elemento tiene el índice 0.
 
-# Rango de indices negativos
+# Rango de indices negativos: Podemos especificar índices negativos si desea comenzar la búsqueda desde el final de la lista:
 
 thislist = ["Manzana", "plátano", "cereza", "naranja", "kiwi", "melon", "mango"]
 print(thislist[-4:-1])
 
 # Este ejemplo devuelve los elementos del índice -4 (incluido) al índice -1 (excluido)
 
-# Cambiar valor del elemento
+# Cambiar valor del elemento: Para cambiar el valor de un elemento específico, consulte el número de índice:
 
 thislist = ["Manzana", "plátano", "cereza"]
 thislist[1] = "Pera"
@@ -20,7 +21,7 @@ print(thislist)
 
 # Cambiar el segundo elemento
 
-# Recorrer una lista
+# Recorrer una lista: Puede recorrer los elementos de la lista utilizando un bucle for
 
 thislist = ["Manzana", "plátano", "cereza"]
 for x in thislist:
@@ -28,7 +29,7 @@ for x in thislist:
 
 # Imprima todos los elementos de la lista, uno por uno
 
-# Comprobar si el elemento existe
+# Comprobar si el elemento existe: Para determinar si un elemento específico está presente en una lista, use la palabra clave in:
 
 thislist = ["Manzana", "plátano", "cereza"]
 if "Manzana" in thislist:
